dc0f2861595c52b060d5a9037da6b787f4706118
TIMUR <timur.sinev2000@mail.ru>
2026-03-13 21:16:19 +0300

    b209v2.1

--- include/tasks.h
+++ include/tasks.h
@@ -1,9 +1,9 @@
 // Copyright 2022 UNN-CS
-#ifndef INCLUDE_TASK_H_
-#define INCLUDE_TASK_H_
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
 
 double taskEarthRope();
 double taskPoolCost();
 double taskFenceCost();
 
-#endif  // INCLUDE_TASK_H_
+#endif // INCLUDE_TASKS_H_
