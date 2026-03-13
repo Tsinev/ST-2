23c7adc03ff19182e57213375803cd1e42a0ab31
TIMUR <timur.sinev2000@mail.ru>
2026-03-13 22:07:03 +0300

    b209v3.0

--- include/tasks.h
+++ include/tasks.h
@@ -2,5 +2,6 @@
 #ifndef INCLUDE_TASKS_H_
 #define INCLUDE_TASKS_H_
 double earthTask();
-double poolTask(double rPool, double widthPath, double priceConcrete, double priceFence);
+double poolTask(double rPool, double widthPath, 
+                double priceConcrete, double priceFence);
 #endif  // INCLUDE_TASKS_H_
